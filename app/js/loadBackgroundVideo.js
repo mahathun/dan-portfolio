bcb255ec659183b2c4cbad1e82d081457d44561f
dan mahavithana <mahathun.online@gmail.com>
2016-12-18 11:52:35 +1300

    Switched to Gulp and added a new workflow

---
+++ app/js/loadBackgroundVideo.js
@@ -0,0 +1,13 @@
+
+  const backgroundVideo = new BackgroundVideo('.bv-video-skill', {
+    src: [
+      '../res/video/back3.mp4'
+
+    ]
+    // ,
+    // onReady: function () {
+    //   // Use onReady() to prevent flickers or force loading state
+    //   const vidParent = document.querySelector(`.${this.bvVideoWrapClass}`);
+    //   vidParent.classList.add('bv-video-wrap--ready');
+    // }
+  });
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,98 @@
-var gulp = require('gulp'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat')
-    plumber = require('gulp-plumber'),
-    imagemin = require('gulp-imagemin');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
+var del = require('del');
+var runSequence = require('run-sequence');
 
-//generate minified concatenated version of the javascript files
-gulp.task('script',function(){
-  gulp.src(['src/js/dist/*.js', 'src/js/*.js', 'src/js/backgroundVideo/backgroundVideo.js'])
-    .pipe(plumber())
-    .pipe(concat('script.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('js/'));
+//compiling sass files
+gulp.task('sass', function(){
+  return gulp.src('app/scss/**/*.scss')
+          .pipe(sass())
+          .pipe(gulp.dest('app/css'))
+          .pipe(browserSync.reload({
+            stream:true
+          }));
+
 });
 
-//generate minified concatenated stylesheet file
-gulp.task('stylesheet', function(){
-  gulp.src('src/css/*.css')
-    .pipe(plumber())
-    .pipe(concat('styles.css'))
-    .pipe(gulp.dest('css/'))
+//optimize images
+gulp.task('images', function(){
+  return gulp.src(['app/img/**/*.+(png|jpg|gif|svg)', '!app/img/originalImages/', '!app/img/originalImages/**/*'])
+          .pipe(cache(imagemin({
+            //Setting interlced to true
+            progressive:true,
+            interlced:true,
+          })))
+          .pipe(gulp.dest('dist/img/'));
 });
 
-// compress images
-gulp.task('image', function(){
-  gulp.src('src/img/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('img'))
+//copying font files
+gulp.task('fonts', function(){
+  return gulp.src('app/fonts/**/*')
+          .pipe(gulp.dest('dist/fonts'));
 });
 
-//watch for changes to js files`
-//watch for changes to css files
-gulp.task('watch', function(){
-  gulp.watch('src/js/**/*.js', ['script']);
-  gulp.watch('src/css/**/*.css', ['stylesheet']);
+//copying video files
+gulp.task('video', function(){
+  return gulp.src('app/res/**/*')
+          .pipe(gulp.dest('dist/res'));
+});
+
+//cleaning up
+//cleant the directory
+gulp.task('clean:dist', function(){
+  return del.sync('dist');
+});
+
+//clear the image cache
+gulp.task('clean:cache', function(callback){
+  return cache.clearAll(callback);
 });
 
 
-gulp.task('default',['script', 'stylesheet']);
+//initializing the development environment
+gulp.task('browserSync',function(){
+  browserSync.init({
+    server:{baseDir:'app'}
+  });
+});
+
+//initializing the production environment
+gulp.task('productionServer', function(){
+  browserSync.init({
+    server:{baseDir:'dist'}
+  });
+});
+
+//copying minified files with html files
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+            .pipe(useref())
+            //minifies only if it's a js file
+            .pipe(gulpIf('*.js', uglify()))
+            //minifies only if it's a css file
+            .pipe(gulpIf('*.css', cssnano()))
+            .pipe(gulp.dest('dist'));
+});
+
+//development workflow : watch tasks
+gulp.task('watch', ['browserSync','sass'],function(){
+  gulp.watch('app/scss/**/*.scss',['sass']);
+  gulp.watch('app/**/*.html',browserSync.reload);
+  gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+//producation workflow
+gulp.task('build',function(callback){
+  runSequence('clean:dist',
+    ['sass','useref','images','fonts', 'video'],
+    'productionServer',
+    callback
+  );
+});
